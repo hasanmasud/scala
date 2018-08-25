6bb5d11475437d4439113a717f3758170d941a92
Masud Hasan <masud.iem@gmail.com>
2018-08-25 20:50:52 +0200

    commit

--- ScalaPractice/src/main/scala/nl/masud/scala3rdedition/controlabstruction/CurrtingTest.scala
+++ ScalaPractice/src/main/scala/nl/masud/scala3rdedition/controlabstruction/CurrtingTest.scala
@@ -2,13 +2,10 @@ package nl.masud.scala3rdedition.controlabstruction
 
 object CurrtingTest {
 
-
   def main(args: Array[String]): Unit = {
-    val addOne = curriedSum(1);
-    print(addOne(2))
+    print(curriedSum(1)(2))
   }
 
-  def curriedSum(x: Int)(y: Int) = (y: Int) => x + y
-
+  def curriedSum(x: Int)(y: Int) = x + y
 
 }
